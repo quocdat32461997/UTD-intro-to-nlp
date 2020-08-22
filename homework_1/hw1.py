72ef8d6b29fdd350e4147763be84c0905cc96405
Eric Ngo <quocdat32461997@yahoo.com>
2020-08-22 11:30:00 -0500

    Finalize data-parsing

--- homework_1/hw1.py
+++ homework_1/hw1.py
@@ -19,46 +19,45 @@ class Person:
 		self.middle_initial = parsed_data[2]
 		self.id = parsed_data[3]
 		self.phone = parsed_data[4]
-		super(Person, self).__init__()
 
-	def get(self):
+	def display(self):
 		"""
 		get - function to return Person object's properties and print Person object
 		"""
 		print(self.last_name, self.first_name, self.middle_initial, self.id, self.phone)
-		return self.last_name, self.first_name, self.middle_initial, self.id, self.phone
+		return self.first_name, self.middle_initial, self.last_name, self.id, self.phone
 
-	def preprocess(self, input):
-		# strip white space
-		input = input.strip()
+	def preprocess(self, inputs):
+		print(inputs)
+		# strip white space, and tokenize by delimiter = ','
+		tokens = inputs.strip().split(',')
 		
-		# tokenize by delimiter = ','
-		tokens = input.split(',')
-
 		# tokens in order: last_name, first_name, middle initial, id, and phone_no
 
 		# processing: last_name, and first name, and middle initial
-		for token in tokens[:3]:
-			if token:
-				token = token.lower()
-				token[0] = token[0].upper()
+		for idx in range(len(tokens[:3])):
+			if tokens[idx]:
+				tokens[idx] = tokens[idx].lower()
+				tokens[idx] = tokens[idx][0].upper() + tokens[idx][1:]
 			else:
-				token = 'X' # if name missing, return X instead
+				tokens[idx] = 'X' # if name missing, return X instead
 
 		# processing: id
-		id_pattern = '/[^A-Za-z][A-Za-z][0-9]{4}/'
+		id_pattern = "^[A-Za-z][A-Za-z][0-9]{4}"
 		while not re.match(id_pattern, tokens[3]):
 			print('ID invalid', tokens[3])
-			tokens[3] = input('ID is two letters followed by 4 digits\nPlease enter a valid id:')
+			print('ID is two letters followed by 4 digits. Please enter a valid id:')
+			tokens[3] = input()
+			tokens[3] = tokens[3].upper()
 
 		# processing phone_no
-		phone_pattern = '/^[0-9]{3}-[0-9]{3}-[0-9]{4}/'
+		phone_pattern = "^[0-9]{3}-[0-9]{3}-[0-9]{4}"
 		if not re.match(phone_pattern, tokens[4]):
 			# remove all non-numeric characters
 			tokens[4] = re.sub('[^0-9]', '', tokens[4])
 
 			# convert to preferred format
-			tokens[4] = str(tokens[4][:3]) + '-' + str(tokens[3:6]) + '-' + str(tokens[6:])
+			tokens[4] = ''.join(tokens[4][:3]) + '-' + ''.join(tokens[4][3:6]) + '-' + ''.join(tokens[4][6:])
 		return tokens
 
 def main():
@@ -67,9 +66,17 @@ def main():
 		data = f.readlines()[1:] # ignore header
 	
 	# loop over to parse multiple Person objects
-	person = []
+	persons = {} 
 	for person in data:
-		persons.append(Person(person))
+		p = Person(person)
+		if not p.id in persons.keys():
+			persons[p.id] = p
+		else:
+			print("Error person {} exists in data file.".format(p.id))
+
+	# loop over to print Person objects
+	for person in persons:
+		person.display()
 
 if __name__ == '__main__':
 	main()
